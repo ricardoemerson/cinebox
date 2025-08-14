2a0805f47fb2746e6648881ff40583c7989b24db
Ricardo Emerson <ricardo_emerson@yahoo.com.br>
2025-08-13 22:09:17 -0300

    feat: update splash screen with new design
    Replaced static text in the splash screen with a new design featuring a background image and logo. Transitioned from `StatelessWidget` to `ConsumerStatefulWidget` for better state management capabilities using Riverpod.

--- lib/ui/splash/splash_page.dart
+++ lib/ui/splash/splash_page.dart
@@ -1,20 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class SplashPage extends StatelessWidget {
+import '../core/themes/resources.dart';
+
+class SplashPage extends ConsumerStatefulWidget {
   const SplashPage({super.key});
 
   @override
+  ConsumerState<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends ConsumerState<SplashPage> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('SplashPage'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'SplashPage is working',
-          style: TextStyle(fontSize: 20),
-        ),
+      body: Stack(
+        children: [
+          Image.asset(
+            R.ASSETS_IMAGES_BG_LOGIN_PNG,
+            fit: BoxFit.cover,
+            width: double.infinity,
+            height: double.infinity,
+          ),
+          SizedBox.expand(
+            child: ColoredBox(
+              color: Colors.black.withAlpha(170),
+            ),
+          ),
+          Center(
+            child: Image.asset(
+              R.ASSETS_IMAGES_LOGO_PNG,
+              width: 200,
+              height: 200,
+            ),
+          ),
+        ],
       ),
     );
   }
