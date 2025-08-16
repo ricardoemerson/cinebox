8870fe2f4716e588041fefb40fc145e9bee9893b
Ricardo Emerson <ricardo_emerson@yahoo.com.br>
2025-08-16 12:59:37 -0300

    feat: add wakelock_plus and cached_network_image packages
    Integrated `wakelock_plus` for managing device screen wakelock functionality and `cached_network_image` for efficient image caching. Updated `pubspec.yaml` and `pubspec.lock` to include these dependencies and their related transitive packages.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:wakelock_plus/wakelock_plus.dart';
 
 import 'cinebox_main_app.dart';
 import 'config/env.dart';
@@ -9,6 +10,8 @@ void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
   GoogleSignIn.instance.initialize(serverClientId: Env.googleApiKey);
+
+  WakelockPlus.enable();
 
   runApp(
     ProviderScope(
