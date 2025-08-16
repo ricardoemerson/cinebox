987db0ede96c24da95b4e11b4a729dd13bc90022
Ricardo Emerson <ricardo_emerson@yahoo.com.br>
2025-08-16 17:53:38 -0300

    feat: add movie models for category and individual items
    Introduced `MovieByCategoryModel` and `MovieModel` classes to structure movie data by categories and individual movie details. These models will support enhanced data organization and flexibility for upcoming features.

---
+++ lib/domain/models/movie_by_category_model.dart
@@ -0,0 +1,15 @@
+import 'Movie_model.dart';
+
+class MovieByCategoryModel {
+  final List<MovieModel> popular;
+  final List<MovieModel> topRated;
+  final List<MovieModel> upcoming;
+  final List<MovieModel> nowPlaying;
+
+  MovieByCategoryModel({
+    required this.popular,
+    required this.topRated,
+    required this.upcoming,
+    required this.nowPlaying,
+  });
+}
