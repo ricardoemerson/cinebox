357eec5f50881ba5b2a1e2f9be88448277ba1ac1
Ricardo Emerson <ricardo_emerson@yahoo.com.br>
2025-08-16 10:21:17 -0300

    refactor: refactor movies app bar layout
    Moved the search bar into the app bar's `PreferredSize` widget for better alignment and spacing. Adjusted widget ordering to improve code readability and maintainability without impacting functionality.

--- lib/ui/movies/widgets/movies_app_bar.dart
+++ lib/ui/movies/widgets/movies_app_bar.dart
@@ -15,6 +15,41 @@ class _MoviesAppBarState extends ConsumerState<MoviesAppBar> {
   Widget build(BuildContext context) {
     return SliverAppBar(
       expandedHeight: MediaQuery.sizeOf(context).height * 0.30,
+      bottom: PreferredSize(
+        preferredSize: const Size.fromHeight(0),
+        child: Padding(
+          padding: const EdgeInsets.all(14.0),
+          child: TextFormField(
+            style: TextStyle(
+              color: Colors.grey[600],
+              fontWeight: FontWeight.w400,
+              fontSize: 14,
+            ),
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: Colors.white,
+              hintText: 'Procurar filme',
+              hintStyle: TextStyle(
+                color: Colors.grey[600],
+                fontSize: 14,
+                fontWeight: FontWeight.w400,
+              ),
+              prefixIcon: Padding(
+                padding: const EdgeInsets.only(left: 8),
+                child: Icon(Icons.search, color: Colors.grey[600]),
+              ),
+              prefixIconConstraints: BoxConstraints(
+                minWidth: 0,
+                minHeight: 0,
+              ),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(25),
+                borderSide: BorderSide.none,
+              ),
+            ),
+          ),
+        ),
+      ),
       foregroundColor: Colors.black,
       surfaceTintColor: Colors.transparent,
       elevation: 0,
@@ -24,35 +59,6 @@ class _MoviesAppBarState extends ConsumerState<MoviesAppBar> {
         background: Image.asset(
           R.ASSETS_IMAGES_SMALL_BANNER_PNG,
           fit: BoxFit.cover,
-        ),
-      ),
-      title: TextFormField(
-        style: TextStyle(
-          color: Colors.grey[600],
-          fontWeight: FontWeight.w400,
-          fontSize: 14,
-        ),
-        decoration: InputDecoration(
-          filled: true,
-          fillColor: Colors.white,
-          hintText: 'Procurar filme',
-          hintStyle: TextStyle(
-            color: Colors.grey[600],
-            fontSize: 14,
-            fontWeight: FontWeight.w400,
-          ),
-          prefixIcon: Padding(
-            padding: const EdgeInsets.only(left: 8),
-            child: Icon(Icons.search, color: Colors.grey[600]),
-          ),
-          prefixIconConstraints: BoxConstraints(
-            minWidth: 0,
-            minHeight: 0,
-          ),
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(25),
-            borderSide: BorderSide.none,
-          ),
         ),
       ),
     );
