ad5d1fc89104703a746b859a75ecf5fbe1acacce
Ricardo Emerson <ricardo_emerson@yahoo.com.br>
2025-08-19 20:25:55 -0300

    feat: enhance FavoritesPage with dynamic data loading
    Replaced placeholder content in `FavoritesPage` with dynamic loading of favorite movies using Riverpod state management. Added a grid view for movie presentation and implemented error handling and loading states. Updated `FavoritesViewModel` to support fetching and removing favorite movies, improving overall user experience and functionality.

--- lib/ui/favorites/favorites_page.dart
+++ lib/ui/favorites/favorites_page.dart
@@ -1,19 +1,85 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class FavoritesPage extends StatelessWidget {
+import '../../domain/models/favorite_movie_model.dart';
+import '../core/widgets/movie_card.dart';
+import 'commands/get_favorites_command.dart';
+import 'favorites_view_model.dart';
+
+class FavoritesPage extends ConsumerStatefulWidget {
   const FavoritesPage({super.key});
 
   @override
+  ConsumerState<FavoritesPage> createState() => _FavoritesPageState();
+}
+
+class _FavoritesPageState extends ConsumerState<FavoritesPage> {
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      ref.read(favoritesViewModelProvider).fetchFavorites();
+    });
+
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final favoritesMovies = ref.watch(getFavoritesCommandProvider);
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('FavoritesPage'),
+        title: const Text('Meus Favoritos'),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text(
-          'FavoritesPage is working',
-          style: TextStyle(fontSize: 20),
+      body: favoritesMovies.when(
+        loading: () => const Center(child: CircularProgressIndicator()),
+        data: (data) {
+          if (data.isEmpty) {
+            return const Center(child: Text('Nenhum filme favorito.'));
+          }
+
+          return Padding(
+            padding: const EdgeInsets.only(bottom: 96, left: 8, right: 8),
+            child: CustomScrollView(
+              slivers: [
+                SliverPadding(padding: const EdgeInsets.symmetric(horizontal: 16)),
+                SliverGrid(
+                  gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                    maxCrossAxisExtent: 148,
+                    mainAxisExtent: 268,
+                  ),
+                  delegate: SliverChildBuilderDelegate(
+                    (context, index) {
+                      final FavoriteMovieModel(:id, :title, :year, posterPath: imageUrl) = data[index];
+
+                      return Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+                        child: MovieCard(
+                          key: UniqueKey(),
+                          id: id,
+                          title: title,
+                          year: year.toString(),
+                          imageUrl: imageUrl,
+                          isFavorite: true,
+                        ),
+                      );
+                    },
+                    childCount: data.length,
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+        error: (error, stackTrace) => Padding(
+          padding: const EdgeInsets.all(20),
+          child: Center(
+            child: Text(
+              'Erro ao buscar filmes favoritos.',
+              style: const TextStyle(color: Colors.red),
+            ),
+          ),
         ),
       ),
     );
--- lib/ui/favorites/favorites_view_model.dart
+++ lib/ui/favorites/favorites_view_model.dart
@@ -1,20 +1,30 @@
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
+import '../../data/repositories/movie/i_movie_repository.dart';
+import '../../data/repositories/repositories_providers.dart';
 import 'commands/get_favorites_command.dart';
 
 part 'favorites_view_model.g.dart';
 
 class FavoritesViewModel {
   final GetFavoritesCommand _getFavoritesCommand;
+  final IMovieRepository _movieRepository;
 
   FavoritesViewModel({
     required GetFavoritesCommand getFavoritesCommand,
-  }) : _getFavoritesCommand = getFavoritesCommand;
+    required IMovieRepository movieRepository,
+  }) : _getFavoritesCommand = getFavoritesCommand,
+       _movieRepository = movieRepository;
 
   Future<void> fetchFavorites() async => _getFavoritesCommand.execute();
+
+  Future<void> removeFavoriteMovie(int movieId) async => _movieRepository.removeFavoriteMovie(movieId);
 }
 
 @riverpod
 FavoritesViewModel favoritesViewModel(Ref ref) {
-  return FavoritesViewModel(getFavoritesCommand: ref.read(getFavoritesCommandProvider.notifier));
+  return FavoritesViewModel(
+    getFavoritesCommand: ref.read(getFavoritesCommandProvider.notifier),
+    movieRepository: ref.read(movieRepositoryProvider),
+  );
 }
