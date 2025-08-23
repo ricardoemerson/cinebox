bf0fdafc41a5ab4bbec9b95b355c633de288be57
Ricardo Emerson <ricardo_emerson@yahoo.com.br>
2025-08-23 13:59:13 -0300

    feat: remove wakelock_plus dependency
    Removed `wakelock_plus` and related transitive dependencies from the project as it is no longer in use. Adjusted `main.dart` to exclude calls to `WakelockPlus`, simplifying initialization logic.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:wakelock_plus/wakelock_plus.dart';
 
 import 'cinebox_main_app.dart';
 import 'config/env.dart';
@@ -10,8 +9,6 @@ void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
   GoogleSignIn.instance.initialize(serverClientId: Env.googleApiKey);
-
-  WakelockPlus.enable();
 
   runApp(
     ProviderScope(
